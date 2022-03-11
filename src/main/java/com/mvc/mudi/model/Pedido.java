7206ecbcff5bbdad953944ff778355a34e15fd69
Felipe <felipeshonorato@gmail.com>
2022-03-11 14:43:50 -0300

    Insert boostrap and make positions

---
+++ src/main/java/com/mvc/mudi/model/Pedido.java
@@ -0,0 +1,61 @@
+package com.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Pedido {
+    private String nomeProduto;
+    private BigDecimal valorNegociado;
+    private LocalDate dataDaEntrega;
+    private String urlProduto;
+    private String urlImagem;
+    private String descricao;
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nome) {
+        this.nomeProduto = nome;
+    }
+
+    public BigDecimal getValorNegociado() {
+        return valorNegociado;
+    }
+
+    public void setValorNegociado(BigDecimal valorNegociado) {
+        this.valorNegociado = valorNegociado;
+    }
+
+    public LocalDate getDataDaEntrega() {
+        return dataDaEntrega;
+    }
+
+    public void setDataDaEntrega(LocalDate dataDaEntrega) {
+        this.dataDaEntrega = dataDaEntrega;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String ulrProduto) {
+        this.urlProduto = ulrProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
